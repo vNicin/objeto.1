81c028a048e7e6118da933320e6bd72a998d4874
Raphael Mathias <rmra2202@gmail.com>
2021-08-01 19:01:53 -0300

    adicionando classe de thread

--- Objeto2Threads/src/objeto2threads/Objeto2Threads.java
+++ Objeto2Threads/src/objeto2threads/Objeto2Threads.java
@@ -21,69 +21,21 @@ public class Objeto2Threads {
      */
     public static void main(String[] args) {
         System.out.println("A linha de chegada é em " + linhaChegada);
-        new Thread(grilo1).start();
-        new Thread(grilo2).start();
-        new Thread(grilo3).start();
+         Grilo grilo1 = new Grilo(1,linhaChegada);
+         new Thread(grilo1).start();
+         Grilo grilo2 = new Grilo(2, linhaChegada);
+         new Thread(grilo2).start();
+         Grilo grilo3 = new Grilo(3, linhaChegada);
+         new Thread(grilo3).start();
+         
+      
          
          
     
         
     }
     
-    
-    private static Runnable grilo1 = new Runnable(){
-        public void run(){
-            int posicao =0;   
-            int numeroPulos=0;
-            do{
-                
-                int pulo = random.nextInt(30);
-                posicao += pulo;
-                numeroPulos++;
-                System.out.println("O Grilo_01 pulou "+ pulo + "cms e já percorreu " + posicao +"cms");
-            }while (posicao<=linhaChegada);
-            if(posicao>linhaChegada){
-                  System.out.println(" O Grilo_01 alcançou a linha de chegada com "+ numeroPulos + " pulos");
-            }
-           
-        }
-    };
-    private static Runnable grilo2 = new Runnable(){
-         public void run(){
-            int posicao =0;   
-            int numeroPulos=0;
-            do{
-                
-                int pulo = random.nextInt(30);
-                posicao += pulo;
-                numeroPulos++;
-                System.out.println("O Grilo_02 pulou "+ pulo + "cms e já percorreu " + posicao +"cms");
-            }while (posicao<=linhaChegada);
-            if(posicao>linhaChegada){
-                  System.out.println(" O Grilo_02 alcançou a linha de chegada com "+ numeroPulos + " pulos");
-            }
-           
-        }
-    };
-       
-        private static Runnable grilo3 = new Runnable(){
-        public void run(){
-            int posicao =0;   
-            int numeroPulos=0;
-            do{
-                
-                int pulo = random.nextInt(30);
-                posicao += pulo;
-                numeroPulos++;
-                System.out.println("O Grilo_03 pulou "+ pulo + "cms e já percorreu " + posicao +"cms");
-            }while (posicao<=linhaChegada);
-            if(posicao>linhaChegada){
-                  System.out.println(" O Grilo_03 alcançou a linha de chegada com "+ numeroPulos + " pulos");
-            }
-           
-        }
-    };
-        
+  
           
     
 }
